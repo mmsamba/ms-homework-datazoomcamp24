5cd03ed20a6579bd5c6838d45e5b6758874cd643
mmsamba <99770695+mmsamba@users.noreply.github.com>
2024-01-29 19:42:14 +0100

    Update variables.tf

--- 01_Homework/variables.tf
+++ 01_Homework/variables.tf
@@ -1,1 +1,41 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/home/msamba/.gc/zoomcamp2024.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
 
+
+variable "project" {
+  description = "Project"
+  default     = "mszoomcamp2024"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default = "europe-west1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default = "EU"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default = "dataset_bq_taxi_ms"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default = "homework-terra-bucket-ms"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
