edfa95218bf108d89d5fb03ee3b551915883bf1d
mmsamba <99770695+mmsamba@users.noreply.github.com>
2024-02-03 17:59:31 +0100

    Add files via upload

---
+++ 02-workflow-orchestration/mage-ai/pipelines/green_taxi_etl/green_load_data_to_gcs_partitionned.py
@@ -0,0 +1,35 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/.gc/mage-zoomcamp-ms-sa-2024-01-29.json'
+bucket_name = 'mage-zoomcamp-ms'
+#object_key = 'nyc_yellow_taxi_data.parquet'
+project_id = 'mszoomcamp2024'
+table_name = 'green_taxi_data' # pyarrom utilise cette table pour partitionner
+
+root_path = f'{bucket_name}/{table_name}'
+
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+
+    # lecture de notre dataframe dans un pyarrow table
+    table = pa.Table.from_pandas(data) # table utilisée par pyarrow
+
+    gcs = pa.fs.GcsFileSystem() # we need the file system object. autozise l'utilisation des variables envionnement automtiquement
+
+    pq.write_to_dataset(
+        table,
+        root_path = root_path,
+        partition_cols = ['lpep_pickup_date'] ,
+        filesystem = gcs
+
+    )
+
+
