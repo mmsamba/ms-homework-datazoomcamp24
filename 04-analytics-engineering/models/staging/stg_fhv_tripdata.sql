d3a8e4fd2c356e2ba68510f7911a1f14677a9f88
mmsamba <99770695+mmsamba@users.noreply.github.com>
2024-02-22 21:24:31 +0100

    Create stg_fhv_tripdata.sql

---
+++ 04-analytics-engineering/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_tripdata') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid,
+        dolocationid,
+        sr_flag,
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
